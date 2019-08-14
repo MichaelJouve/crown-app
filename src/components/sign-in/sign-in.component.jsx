f785347b2dc039c848c839e0cccd973f6361507b
MJ <michaeljouve@gmail.com>
2019-08-14 10:57:04 +0200

    finished sign up and in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils'
 
 
 import "./sign-in.styles.scss";
@@ -16,8 +16,11 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    const { email, password } = this.state;
+
+    await auth.signInWithEmailAndPassword(email, password);
     this.setState({ email: "", password: "" });
   };
 
