6fda9030eaa4e5ea4f7f94313dfd4a0097f24957
MJ <michaeljouve@gmail.com>
2019-08-21 10:58:43 +0200

    add and remove item from cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,5 +1,7 @@
 export const addItemToCart = (cartItems, cartItemsToAdd) => {
-  const existingCartItem = cartItems.find(cartItem => cartItemsToAdd.id === cartItem.id);
+  const existingCartItem = cartItems.find(
+    cartItem => cartItemsToAdd.id === cartItem.id
+  );
 
   if (existingCartItem) {
     return cartItems.map(cartItem =>
@@ -10,7 +12,23 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
           }
         : cartItem
     );
-  };
+  }
 
-  return [...cartItems, {...cartItemsToAdd, quantity: 1 }]
+  return [...cartItems, { ...cartItemsToAdd, quantity: 1 }];
+};
+
+export const removeItemToCart = (cartItems, cartItemsToRemove) => {
+  if (cartItemsToRemove.quantity > 1) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemsToRemove.id
+        ? {
+            ...cartItem,
+            quantity: cartItem.quantity - 1
+          }
+        : cartItem
+    );
+  } else {
+    // remove item to cart
+    return cartItems.filter(cartItem => cartItem.id !== cartItemsToRemove.id);
+  }
 };
